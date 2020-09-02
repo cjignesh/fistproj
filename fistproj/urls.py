eea6d0635a6baacbcb4baa08e52dc381cb5f73e2
jignsh chauhan <cjignesh>
2020-09-02 11:44:49 +0530

    Course completed

--- fistproj/urls.py
+++ fistproj/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include('travelo.urls')),
     path("calc/", include('firstapp.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
